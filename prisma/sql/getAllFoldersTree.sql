9cc28b05d2630ef91bfb47a7a9b8a284743360c4
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-08-25 00:49:50 +0200

    Revert changes to raw sql queries
    Added back the schema name in the queries

--- prisma/sql/getAllFoldersTree.sql
+++ prisma/sql/getAllFoldersTree.sql
@@ -1,12 +1,12 @@
 WITH RECURSIVE homeFolders AS (
     SELECT id, name, created_at, type, owner_id, parent_id, 0 AS level
-    FROM files
+    FROM file_uploader.files
     WHERE parent_id IS NULL AND type = 'FOLDER'
 ), goDown AS (
     TABLE homeFolders
     UNION ALL
     (SELECT this.id, this.name, this.created_at, this.type, this.owner_id, this.parent_id, previous.level + 1
-    FROM files this
+    FROM file_uploader.files this
     INNER JOIN goDown previous ON this.parent_id = previous.id
     WHERE this.type = 'FOLDER')
 )
@@ -20,6 +20,6 @@ SELECT
     ft.parent_id AS "parentId",
     ft.level AS "level"
 FROM goDown ft
-LEFT JOIN files p ON ft.parent_id = p.id
-LEFT JOIN users u ON ft.owner_id = u.id
+LEFT JOIN file_uploader.files p ON ft.parent_id = p.id
+LEFT JOIN file_uploader.users u ON ft.owner_id = u.id
 ORDER BY ft.level;
